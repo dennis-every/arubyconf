eaef39da2c4c22105cc1ad777ca9238096ced0d3
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-03 22:14:05 -0400

    Add JS mobile menu link handler and rel="noopener noreferrer" to target _blank links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenuBtn = document.getElementById('close-menu-btn');
+const mobileLinks = document.getElementById('mobile-links');
+
+const preventDefaultHandler = (e) => {
+  e.preventDefault();
+};
+
+function openMenuHandler() {
+  mobileMenu.classList.replace('d-none', 'd-flex');
+  document.body.classList.add('stop-scrolling');
+  document.body.addEventListener('touchmove', preventDefaultHandler);
+}
+
+function closeMenuHanlder() {
+  mobileMenu.classList.replace('d-flex', 'd-none');
+  document.body.classList.remove('stop-scrolling');
+  document.body.removeEventListener('touchmove', preventDefaultHandler);
+}
+
+menuBtn.addEventListener('click', openMenuHandler);
+closeMenuBtn.addEventListener('click', closeMenuHanlder);
+mobileLinks.addEventListener('click', closeMenuHanlder);
